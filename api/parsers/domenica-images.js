2d05c0ab40041f938413b303c65b455600eb7891
martinmoscinski-cmyk <martin.moscinski@gmail.com>
2026-05-28 21:44:56 +0200

    Add files via upload

---
+++ api/parsers/domenica-images.js
@@ -0,0 +1,398 @@
+export default {
+  "absolute": {
+    "cover": "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/01.jpg",
+    "images": [
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/01.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/02.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/03.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/04.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/05.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/06.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/07.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/08.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/09.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/EXTERNAL/10.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/01.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/02.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/03.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/04.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/05.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/06.jpg",
+      "/images/DOMENICA/ABSOLUTE/RENDERS/INTERNAL/07.jpg"
+    ]
+  },
+  "aqua": {
+    "cover": "/images/DOMENICA/AQUA/RENDERS/AQUA1.jpg",
+    "images": [
+      "/images/DOMENICA/AQUA/RENDERS/AQUA1.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA10.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA11.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA12.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA13.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA14.jpeg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA15.jpeg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA16.jpeg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA17.jpeg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA18.jpeg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA19.jpeg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA2.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA3.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA4.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA5.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA6.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA7.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA8.jpg",
+      "/images/DOMENICA/AQUA/RENDERS/AQUA9.jpg"
+    ]
+  },
+  "artemis": {
+    "cover": "/images/DOMENICA/ARTEMIS/RENDERS/Day1.jpg",
+    "images": [
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day1.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day2.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day3.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day4.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day5.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day6.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Day7.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon1.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon2.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon3.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon4.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon5.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon6.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/Afternoon7.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/04.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/08.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/09.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/11.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/12.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/13.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/DJI_0636.jpg",
+      "/images/DOMENICA/ARTEMIS/RENDERS/DJI_0637.jpg"
+    ]
+  },
+  "cirvis": {
+    "cover": "/images/DOMENICA/CIRVIS/RENDERS/DAY/01.jpg",
+    "images": [
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/01.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/02.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/03.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/04.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/05.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/06.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/07.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/08.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/09.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/10.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/11.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/DAY/12.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis0.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis1.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis2.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis3.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis4.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis5.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis6.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis7.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis8.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/AFT/cirvis9.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/INT/01.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/INT/02.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/INT/03.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/INT/04.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/INT/05.jpg",
+      "/images/DOMENICA/CIRVIS/RENDERS/INT/06.jpg"
+    ]
+  },
+  "eniko-mare": {
+    "cover": "/images/DOMENICA/ENIKO MARE/RENDERS/01.jpg",
+    "images": [
+      "/images/DOMENICA/ENIKO MARE/RENDERS/01.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/02.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/03.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/04.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/05.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/06.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/07.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/08.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/09.jpg",
+      "/images/DOMENICA/ENIKO MARE/RENDERS/10.jpg"
+    ]
+  },
+  "konia-aura": {
+    "cover": "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA0.jpg",
+    "images": [
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA0.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA1.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA10.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA11.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA12.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA13.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA14.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA15.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA2.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA3.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA4.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA5.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA6.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA7.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA8.jpg",
+      "/images/DOMENICA/KONIA AURA/RENDERS/KONIA AURA9.jpg"
+    ]
+  },
+  "la-bella": {
+    "cover": "/images/DOMENICA/LA BELLA/RENDERS/02.jpeg",
+    "images": [
+      "/images/DOMENICA/LA BELLA/RENDERS/02.jpeg"
+    ]
+  },
+  "lyra": {
+    "cover": "/images/DOMENICA/LYRA/RENDERS/01.jpg",
+    "images": [
+      "/images/DOMENICA/LYRA/RENDERS/01.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/010.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/011.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/012.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/02.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/03.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/04.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/05.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/06.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/07.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/08.jpg",
+      "/images/DOMENICA/LYRA/RENDERS/09.jpg"
+    ]
+  },
+  "lyra-ii": {
+    "cover": "/images/DOMENICA/LYRA II/RENDERS/1.jpg",
+    "images": [
+      "/images/DOMENICA/LYRA II/RENDERS/1.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/10.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/11.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/12.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/13.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/2.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/3.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/4.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/5.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/6.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/7.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/8.jpg",
+      "/images/DOMENICA/LYRA II/RENDERS/9.jpg"
+    ]
+  },
+  "mare": {
+    "cover": "/images/DOMENICA/MARE/RENDERS/Exteriors/001.JPG",
+    "images": [
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/001.JPG",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/002.JPG",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/01.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/02.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/03.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/04.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/05.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/Mare2.JPG",
+      "/images/DOMENICA/MARE/RENDERS/Exteriors/Mare3.JPG",
+      "/images/DOMENICA/MARE/RENDERS/Interiors/01.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Interiors/02.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Interiors/03.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Interiors/04.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Interiors/05.jpg",
+      "/images/DOMENICA/MARE/RENDERS/Interiors/06.jpg"
+    ]
+  },
+  "montes": {
+    "cover": "/images/DOMENICA/MONTES/RENDERS/01 13.jpg",
+    "images": [
+      "/images/DOMENICA/MONTES/RENDERS/01 13.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/01.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/02 14.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/02.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/03 15.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/03.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/04.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/05 16.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/05.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/06 17.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/06.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/07 18.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/07.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/08.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/09.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/10.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/11.jpg",
+      "/images/DOMENICA/MONTES/RENDERS/12.jpg"
+    ]
+  },
+  "oculus": {
+    "cover": "/images/DOMENICA/OCULUS/RENDERS/DAY/01.jpg",
+    "images": [
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/01.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/02.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/03.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/04.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/05.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/06.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/07.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/08.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/09.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/DAY/10.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/01.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/02.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/03.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/04.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/05.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/06.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/07.jpg",
+      "/images/DOMENICA/OCULUS/RENDERS/AFTERNOON/08.jpg"
+    ]
+  },
+  "orion": {
+    "cover": "/images/DOMENICA/ORION/RENDERS/exterior/Orion1.jpg",
+    "images": [
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion1.jpg",
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion2.jpg",
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion3.jpg",
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion4.jpg",
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion5.jpg",
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion6.jpg",
+      "/images/DOMENICA/ORION/RENDERS/exterior/Orion7.jpg",
+      "/images/DOMENICA/ORION/RENDERS/interior/Orion1.jpg",
+      "/images/DOMENICA/ORION/RENDERS/interior/Orion2.jpg",
+      "/images/DOMENICA/ORION/RENDERS/interior/Orion3.jpg",
+      "/images/DOMENICA/ORION/RENDERS/interior/Orion4.jpg",
+      "/images/DOMENICA/ORION/RENDERS/interior/Orion5.jpg"
+    ]
+  },
+  "premier": {
+    "cover": "/images/DOMENICA/PREMIER/RENDERS/01.png",
+    "images": [
+      "/images/DOMENICA/PREMIER/RENDERS/01.png",
+      "/images/DOMENICA/PREMIER/RENDERS/03.png",
+      "/images/DOMENICA/PREMIER/RENDERS/05.png",
+      "/images/DOMENICA/PREMIER/RENDERS/06.png",
+      "/images/DOMENICA/PREMIER/RENDERS/CAM_03_.png",
+      "/images/DOMENICA/PREMIER/RENDERS/CAM_09_BEDROOM.jpg",
+      "/images/DOMENICA/PREMIER/RENDERS/CAM_10_LIVING.jpg",
+      "/images/DOMENICA/PREMIER/RENDERS/CAM_11_KITCHEN.jpg"
+    ]
+  },
+  "quattro": {
+    "cover": "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/01.jpg",
+    "images": [
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/01.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/02.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/03.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/04.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/05.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/06.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/07.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/08.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/09.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/10.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/11.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/12.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Exterior/DAY/13.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/01.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/02 2.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/02.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/03.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/04 2.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/04.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/05 2.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/05.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/06.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/07.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/08.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/09.jpg",
+      "/images/DOMENICA/QUATTRO/RENDERS/Interiors/10.jpg"
+    ]
+  },
+  "riverside": {
+    "cover": "/images/DOMENICA/RIVERSIDE/RENDERS/1.jpg",
+    "images": [
+      "/images/DOMENICA/RIVERSIDE/RENDERS/1.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/2.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/3.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/4.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/5.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/6.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/7.jpg",
+      "/images/DOMENICA/RIVERSIDE/RENDERS/8.jpg"
+    ]
+  },
+  "thea": {
+    "cover": "/images/DOMENICA/THEA/RENDERS/01.jpg",
+    "images": [
+      "/images/DOMENICA/THEA/RENDERS/01.jpg",
+      "/images/DOMENICA/THEA/RENDERS/02.jpg",
+      "/images/DOMENICA/THEA/RENDERS/03.jpg",
+      "/images/DOMENICA/THEA/RENDERS/04.jpg",
+      "/images/DOMENICA/THEA/RENDERS/05.jpg",
+      "/images/DOMENICA/THEA/RENDERS/06.jpg",
+      "/images/DOMENICA/THEA/RENDERS/07.jpg",
+      "/images/DOMENICA/THEA/RENDERS/08.jpg",
+      "/images/DOMENICA/THEA/RENDERS/09.jpg",
+      "/images/DOMENICA/THEA/RENDERS/10.jpg",
+      "/images/DOMENICA/THEA/RENDERS/11.jpg",
+      "/images/DOMENICA/THEA/RENDERS/12.jpg",
+      "/images/DOMENICA/THEA/RENDERS/13.jpg",
+      "/images/DOMENICA/THEA/RENDERS/14.jpg",
+      "/images/DOMENICA/THEA/RENDERS/15.jpg",
+      "/images/DOMENICA/THEA/RENDERS/Thea16.jpg",
+      "/images/DOMENICA/THEA/RENDERS/Thea17.jpg",
+      "/images/DOMENICA/THEA/RENDERS/Thea18.jpg",
+      "/images/DOMENICA/THEA/RENDERS/Thea19.jpg"
+    ]
+  },
+  "uptown": {
+    "cover": "/images/DOMENICA/UPTOWN/RENDERS/01.jpg",
+    "images": [
+      "/images/DOMENICA/UPTOWN/RENDERS/01.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/02.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/03.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/04.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/05.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/06.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/07.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/08.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/09.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/10 copy.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/10.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/11.jpg",
+      "/images/DOMENICA/UPTOWN/RENDERS/12.jpg"
+    ]
+  },
+  "virgo": {
+    "cover": "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/01.jpg",
+    "images": [
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/01.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/02.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/06.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/07.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/10.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/11.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/12.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/13.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/16.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/4.jpg",
+      "/images/DOMENICA/VIRGO/APARTMENTS/RENDERS/9.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/01.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/02.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/03.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/04.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/05.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/06.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/07.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/08.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/10.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/11.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/12.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/13.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/16.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/4.jpg",
+      "/images/DOMENICA/VIRGO/VILLAS/RENDERS/9.jpg"
+    ]
+  }
+};
