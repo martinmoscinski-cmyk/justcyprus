b9a42ded3348bdb4a594a53c6081b399a5449565
martinmoscinski-cmyk <martin.moscinski@gmail.com>
2026-05-30 13:05:37 +0200

    Add files via upload

--- api/parsers/giovani.js
+++ api/parsers/giovani.js
@@ -1,4 +1,20 @@
-import { giovaniData } from "./giovani-data.js";
+const BLOB_URL =
+  "https://e3ysmayz4e4ch5bu.public.blob.vercel-storage.com/data/giovani-data.json";
+
+export async function getGiovaniProjects() {
+  try {
+    const response = await fetch(BLOB_URL);
+
+    if (!response.ok) {
+      return [];
+    }
+
+    return await response.json();
+
+  } catch {
+    return [];
+  }
+}import { giovaniData } from "./giovani-data.js";
 
 export async function getGiovaniProjects() {
   return giovaniData;
